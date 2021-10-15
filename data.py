25b681ac4ba49875dc01b4c07e525151cc497f54
Janek <jan.piotr.latko@gmail.com>
2021-10-15 18:02:48 +0200

    normalization

--- data.py
+++ data.py
@@ -36,10 +36,10 @@ def set_up_data(H):
         H.image_size = 256
         H.image_channels = 3
         # TODO: ?
-        # shift = -112.8666757481
-        # scale = 1. / 69.84780273
-        shift = -0.4387
-        scale = 1.0 / 0.2743
+        shift = -112.8666757481
+        scale = 1. / 69.84780273
+        # shift = -0.4387
+        # scale = 1.0 / 0.2743
     elif H.dataset == 'ffhq_1024':
         trX, vaX, teX = ffhq1024(H.data_root)
         H.image_size = 1024
