515b3fd6a83253f5037704f85f90df380f81e024
Janek <jan.piotr.latko@gmail.com>
2021-10-15 23:50:51 +0200

    latent classification

--- classify_latents.py
+++ classify_latents.py
@@ -4,24 +4,34 @@ os.environ["NUMEXPR_NUM_THREADS"] = "1"
 os.environ["OMP_NUM_THREADS"] = "1"
 
 import numpy as np
-from latents import get_latents
+from latents import get_latents, get_available_latents
 # TODO: ?
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 
-
-def main():
-    z, meta = get_latents(latents_dir="/scratch/s193223/vdvae/latents/", layer_ind=0, splits=[1])
-    col = "Young"
-    y = np.array(meta[col]==1)
+def get_classification_score(col, layer_ind):
+    print(layer_ind, col)
+    # TODO: split?
+    z, meta = get_latents(latents_dir="/scratch/s193223/vdvae/latents/", layer_ind=layer_ind, splits=[1])
+    print(z.shape)
+    y = np.array(meta[col] == 1)
     z = z.reshape(z.shape[0], -1)
+    # TODO: split?
     X_train, X_test, y_train, y_test = train_test_split(z, y, test_size=0.33, stratify=y, random_state=42)
 
     neigh = KNeighborsClassifier(n_neighbors=5, n_jobs=8)
 
     neigh.fit(X_train, y_train)
 
-    print(neigh.score(X_test, y_test))
+    return neigh.score(X_test, y_test)
+
+def main():
+    col = "Young"
+    latent_ids = get_available_latents()
+    print(latent_ids)
+    for i in latent_ids:
+        score = get_classification_score(col, i)
+        print(i, score)
 
 if __name__ == "__main__":
     main()
--- latents.py
+++ latents.py
@@ -6,6 +6,12 @@ from tqdm import tqdm
 CELEBAHQ_DIR = "/scratch/s193223/celebahq2/CelebAMask-HQ/"
 
 
+def get_available_latents(latents_dir="/scratch/s193223/vdvae/latents/"):
+    fname = os.listdir(latents_dir)[0]
+    keys = list(np.load(os.path.join(latents_dir, fname)))
+    latent_ids = list(sorted(set(int(k.split("_")) for k in keys)))
+    return latent_ids
+
 def get_latents(latents_dir, layer_ind, splits=(1,2,3), root_dir=CELEBAHQ_DIR):
     metadata = pd.read_csv(os.path.join(root_dir, 'metadata.csv'))
     metadata = metadata[metadata.split.isin(splits)]
