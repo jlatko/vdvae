c5979048fccdfec47c56b120e07dad8ad1af4fec
Janek <jan.piotr.latko@gmail.com>
2021-10-17 21:28:30 +0200

    swap

--- visualize_from_representations.py
+++ visualize_from_representations.py
@@ -82,7 +82,7 @@ def swap(H, idx, idx2, ema_vae, latent_ids):
         lv_points = np.floor(np.linspace(0, 1, H.num_variables_visualize + 2) * len(zs)).astype(int)[1:-1]
         im = np.zeros(shape=(H.num_variables_visualize * 256, H.num_variables_visualize * 256, 3))
         for i in range(H.num_variables_visualize):
-            for j in range(j, H.num_variables_visualize):
+            for j in range(i, H.num_variables_visualize):
                 zs_current = copy(zs)
                 print((i,j), (lv_points[i], lv_points[j]))
                 zs_current[lv_points[i]:lv_points[j]] = zs2[lv_points[i]:lv_points[j]]
