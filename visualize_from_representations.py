b44be20771b3f672eef6ae83c92775e844aada68
Janek <jan.piotr.latko@gmail.com>
2021-10-17 19:07:08 +0200

    visualize

--- visualize_from_representations.py
+++ visualize_from_representations.py
@@ -26,16 +26,18 @@ def reconstruct_image(H, idx, ema_vae, latent_ids):
         zs = [torch.tensor(z_dict[f'z_{i}'][np.newaxis], dtype=torch.float32).cuda() for i in latent_ids]
         lv_points = np.floor(np.linspace(0, 1, H.num_variables_visualize + 2) * len(zs)).astype(int)[1:-1]
         print(lv_points)
-        batches = []
-        for i in lv_points:
-            # create reconstructions using i first latents and low temperature (0.1)
-            batches.append(ema_vae.forward_samples_set_latents(1, zs[:i], t=0.1))
-        n_rows = len(batches)
-        im = np.concatenate(batches, axis=0).reshape((n_rows, 1, *batches[0].shape[1:])).transpose(
-            [0, 2, 1, 3, 4]).reshape([n_rows * batches[0].shape[1], batches[0].shape[2], 3])
+        for t in [0.1, 0.5, 0.8, 1]:
+            batches = []
+            for i in lv_points:
+                # create reconstructions using i first latents and low temperature (0.1)
+                batches.append(ema_vae.forward_samples_set_latents(1, zs[:i], t=0.1))
+            n_rows = len(batches)
+            im = np.concatenate(batches, axis=0).reshape((n_rows, 1, *batches[0].shape[1:])).transpose(
+                [0, 2, 1, 3, 4]).reshape([n_rows * batches[0].shape[1], batches[0].shape[2], 3])
 
-        fname = os.path.join(H.destination_dir, f"{idx}.png")
-        imageio.imwrite(fname, im)
+            fname = os.path.join(H.destination_dir, f"{idx}_{str(t).replace('.','_')}.png")
+            imageio.imwrite(fname, im)
+
 
 def main():
     H, logprint = set_up_hyperparams(extra_args_fn=add_params)
