cd6175a120dc67c98354c9b5a1ce5e5ad74db096
Janek <jan.piotr.latko@gmail.com>
2021-10-21 18:28:04 +0200

    classify config

--- classify_latents.py
+++ classify_latents.py
@@ -53,8 +53,8 @@ def get_classification_score(H, X_train, X_test, y_train, y_test):
         'roc_auc_score': roc_auc_score(y_test, y_pred),
     }
 
-def run_classifications(H, cols, layer_ind):
-    z, meta = get_latents(latents_dir="/scratch/s193223/vdvae/latents/", layer_ind=layer_ind, splits=[1,2,3], allow_missing=False)
+def run_classifications(H, cols, layer_ind, latents_dir):
+    z, meta = get_latents(latents_dir=latents_dir, layer_ind=layer_ind, splits=[1,2,3], allow_missing=False)
     logging.debug(z.shape)
 
     resolution = z.shape[-2]
@@ -95,7 +95,7 @@ def parse_args(s=None):
     H = Hyperparams()
     parser = argparse.ArgumentParser()
 
-
+    parser.add_argument('--latents_dir', type=str, default="/scratch/s193223/vdvae/latents/")
     parser.add_argument('--keys_set', type=str, default='small')
     parser.add_argument('--layer_ids_set', type=str, default='small')
     parser.add_argument('--log_level', type=str, default='INFO')
@@ -110,9 +110,57 @@ def setup(H):
         cols = ["Young", "Male",  "Smiling", "Wearing_Earrings", "Brown_Hair"]
     elif H.keys_set == "big":
         cols = ["Young", "Male", "Bald", "Mustache", "Smiling", "Chubby",
-                "Attractive", "Brown_Hair", "Blond_Hair", "Bushy_Eyebrows",
-                "Wearing_Earrings", "Heavy_Makeup", "Mouth_Slightly_Open",
-                "Narrow_Eyes", "Big_Lips", "Big_Nose", "Eyeglasses", "Wearing_Lipstick"]
+                "Straight_Hair", "Wavy_Hair", "Bangs",
+                "Black_Hair", "Brown_Hair", "Blond_Hair",
+                "Attractive",
+                "Mouth_Slightly_Open",
+                "Narrow_Eyes", "Bushy_Eyebrows",
+                "Oval_Face", "Big_Lips", "Big_Nose", "Pointy_Nose",
+                "Eyeglasses",
+                "Heavy_Makeup", "Pale_Skin"
+                "Wearing_Hat", "Wearing_Earrings", "Wearing_Lipstick"]
+    elif H.keys_set == "full":
+        cols = ['5_o_Clock_Shadow',
+         'Arched_Eyebrows',
+         'Attractive',
+         'Bags_Under_Eyes',
+         'Bald',
+         'Bangs',
+         'Big_Lips',
+         'Big_Nose',
+         'Black_Hair',
+         'Blond_Hair',
+         'Blurry',
+         'Brown_Hair',
+         'Bushy_Eyebrows',
+         'Chubby',
+         'Double_Chin',
+         'Eyeglasses',
+         'Goatee',
+         'Gray_Hair',
+         'Heavy_Makeup',
+         'High_Cheekbones',
+         'Male',
+         'Mouth_Slightly_Open',
+         'Mustache',
+         'Narrow_Eyes',
+         'No_Beard',
+         'Oval_Face',
+         'Pale_Skin',
+         'Pointy_Nose',
+         'Receding_Hairline',
+         'Rosy_Cheeks',
+         'Sideburns',
+         'Smiling',
+         'Straight_Hair',
+         'Wavy_Hair',
+         'Wearing_Earrings',
+         'Wearing_Hat',
+         'Wearing_Lipstick',
+         'Wearing_Necklace',
+         'Wearing_Necktie',
+         'Young']
+
     else:
         raise ValueError(f"Unknown keys set {H.keys_set}")
 
@@ -152,7 +200,7 @@ def main():
 
     scores = defaultdict(list)
     for i in tqdm(latent_ids):
-        score_dict = run_classifications(H, cols, i)
+        score_dict = run_classifications(H, cols, i, latents_dir=H.latents_dir)
         for col, score in score_dict.items():
             scores[col].append(score)
 
