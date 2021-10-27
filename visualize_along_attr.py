a940c42a8e48bc9d9010b33d9537f295465bc2a0
Janek <jan.piotr.latko@gmail.com>
2021-10-27 20:31:58 +0200

    fix

--- visualize_along_attr.py
+++ visualize_along_attr.py
@@ -25,7 +25,7 @@ wandb.config.update({"script": "vis_attr"})
 def add_params(parser):
     parser.add_argument('--latents_dir', type=str, default='/scratch/s193223/vdvae/latents/')
     parser.add_argument('--attr_means_dir', type=str, default='/scratch/s193223/vdvae/attr_means/')
-    parser.add_argument('--norm', type=str, default=None, help="None|channel|pixel")
+    parser.add_argument('--norm', type=str, default="pixel", help="none|channel|pixel")
     parser.add_argument('--n_samples', type=int, default=1)
     parser.add_argument('--size', type=int, default=128)
     parser.add_argument('--n_steps', type=int, default=13)
@@ -117,6 +117,8 @@ def main():
     lv_points = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14, 16, 18, 20, 21, 24, 27, 30, 33, 36, 39, 43, 48, 53, 58, 63]
 
     wandb.config.update(H)
+    wandb.config.update({"attributes": attributes, "latent_ids": latent_ids, "lv_points": lv_points})
+
     print(lv_points)
     for i in range(H.n_samples):
         idx = data_valid_or_test.metadata.iloc[i].idx
