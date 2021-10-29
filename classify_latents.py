0756130bc6512965a38010e8c1d526b327ffae59
Janek <jan.piotr.latko@gmail.com>
2021-10-29 15:46:28 +0200

    grouping

--- classify_latents.py
+++ classify_latents.py
@@ -198,7 +198,7 @@ def run_classifications(H, cols, layer_ind, latents_dir, handle_nan=False, cuda=
         scores.update(group_scores(scores_male, scores_female, cols_filtered, layer_ind))
 
         #
-        scores["Male"] = get_all_scores(H, z, meta, ["Male"], cuda, layer_ind)["Male"]
+        # scores["Male"] = get_all_scores(H, z, meta, ["Male"], cuda, layer_ind)["Male"]
 
     else:
         scores.update(get_all_scores(H, z, meta, list(cols_filtered), cuda, layer_ind))
@@ -279,6 +279,8 @@ def main():
     # path = f"outputs/"
     # if not os.path.exists(path):
     #     os.makedirs(path)
+    if H.grouped:
+        cols = [c for c in cols if c != "Male"] # filter out Male as we group by sex
 
     logging.info(cols)
     logging.info(latent_ids)
