7a71fd61dcf149ed023f57e5f160bd42bce27e62
Janek <jan.piotr.latko@gmail.com>
2021-11-12 14:14:09 +0100

    i256

--- imagenet256.py
+++ imagenet256.py
@@ -5,7 +5,7 @@ from PIL import Image
 import torch
 import pandas as pd
 from torch.utils.data import Dataset
-
+from tqdm import tqdm
 
 I256_DIR = "/scratch/s193223/inet/"
 
@@ -22,8 +22,21 @@ class ImageNet256Dataset(Dataset):
         self.root_dir = root_dir
         self.transform = transform
         self.files = list(glob(root_dir + "/*.JPEG"))
+
+        self.prefilter_bnw()
+
         if len(self.files) == 0:
             raise RuntimeError(f"No files matching {root_dir}/*.JPEG")
+
+    def prefilter_bnw(self):
+        files = []
+        print("filtering files")
+        for f in tqdm(self.files):
+            img = Image.open(f)
+            if img.mode == "RGB":
+                files.append(f)
+        print(f"left: {len(files)}/{len(self.files)}")
+        self.files = files
 
     def __len__(self):
         return len(self.files)
