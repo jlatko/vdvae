77ec558bb5ccbd3eaaec876448e9cdae42717b19
Janek <jan.piotr.latko@gmail.com>
2021-11-18 19:33:57 +0100

    minor improvements

--- visualize_along_dim.py
+++ visualize_along_dim.py
@@ -151,11 +151,11 @@ def plot_metrics(dims):
     for i in range(l):
         for k in dims["keys"]:
             wandb.log({
-                f"{k}_explained_var": dims[f"{k}_explained_var"],
-                # f"{k}_explained_var1": dims[f"{k}_explained_var1"],
-                # f"{k}_explained_var2": dims[f"{k}_explained_var2"],
-                # f"{k}_covariance_12": dims[f"{k}_covariance_12"],
-                f"{k}_correlation_12": dims[f"{k}_correlation_12"],
+                f"{k}_explained_var": dims[f"{k}_explained_var"][i],
+                f"{k}_explained_var1": dims[f"{k}_explained_var1"][i],
+                f"{k}_explained_var2": dims[f"{k}_explained_var2"][i],
+                f"{k}_covariance_12": dims[f"{k}_covariance_12"][i],
+                f"{k}_correlation_12": dims[f"{k}_correlation_12"][i],
             }, step=i)
 
 def main():
