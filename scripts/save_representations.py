9f8542c713642aae7bbd305579352ca1ba0dbde9
Janek <jan.piotr.latko@gmail.com>
2022-02-15 10:38:45 +0100

    cont

--- scripts/save_representations.py
+++ scripts/save_representations.py
@@ -25,6 +25,7 @@ def save_repr(H, ema_vae, data_valid, preprocess_fn, keys=("z", "kl", "qm", "qv"
                 if not os.path.exists(os.path.join(H.destination_dir, f"{idx}.npz")):
                     all_present = False
             if all_present:
+                print("present, skipping")
                 continue
 
         data_input, target = preprocess_fn(x)
